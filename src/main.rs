90bc230ba9007ff645833396b125c631e885e555
Davide Olgiati <davide.olgiati@hotmail.it>
2025-07-25 09:01:11 +0200

    ui improvements

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,16 @@ use crate::cli_args::CliArgs;
 use crate::flac_file::FlacFile;
 use crate::output_format::OutputFormat;
 
+fn format_db(value: f32) -> String {
+    if value > 0.0 {
+        format!("+{:.2} dB", value)
+    } else if value == 0.0 {
+        "0.00 dB".to_string()
+    } else {
+        format!("{:.2} dB", value)
+    }
+}
+
 fn print_file_details(filename: &String, file: &FlacFile) {
     let seconds = file.duration() % 60_f32;
     let minutes = (file.duration() - seconds) / 60_f32;
@@ -29,27 +39,26 @@ fn print_file_details(filename: &String, file: &FlacFile) {
     println!("── FILE DETAILS {}\n", "─".repeat(54));
     println!("   {:<18} : {}", "Filename", *filename);
     println!("   {:<18} : {:02.0}:{:02.0}", "Duration", minutes, seconds);
-    println!("   {:<18} : {} bit / {:.1} kHz", "Format", file.bit_depth(), file.sample_rate() as f32 / 1000.0);
+    println!("   {:<18} : {} bit / {} Hz", "Format", file.bit_depth(), file.sample_rate());
     println!("   {:<18} : {}", "Channels", file.channel_count());
     println!("   {:<18} : {}\n", "Sample Count", file.samples_count());
     
     println!("\n── STEREO FIELD ANALYSIS {}\n", "─".repeat(45));
-    println!("   {:<18} : {:.2} dB", "RMS Balance (L/R)", file.rms_balance());
-    println!("   {:<18} : {:.2}\n", "Stereo Correlation", file.stereo_correlation());
+    println!("   {:<18} : {}", "RMS Balance (L/R)", format_db(file.rms_balance()));
+    println!("   {:<18} :  {:.2}\n", "Stereo Correlation", file.stereo_correlation());
     
     println!("\n{}", "─".repeat(70));
-    println!("       {:<23} |      {:>8}  |     {:>8}", "CHANNEL ANALYSIS", "LEFT", "RIGHT");
+    println!("      {:<23}|   {:>12}   |   {:>12}", "CHANNEL ANALYSIS", "LEFT", "RIGHT");
     println!("{}", "─".repeat(70));
-    
-    println!("       {:<23} |   {:>8.2} dB  |  {:>8.2} dB", "RMS Level", left.rms(), right.rms());
-    println!("       {:<23} |   {:>8.2} dB  |  {:>8.2} dB", "Peak Level", left.peak(), right.peak());
-    println!("       {:<23} |   {:>8.2} dB  |  {:>8.2} dB", "True Peak", left.true_peak(), right.true_peak());
-    println!("       {:<23} |   {:>8.2} dB  |  {:>8.2} dB", "Crest Factor", left.crest_factor(), right.crest_factor());
-    println!("       {:<23} |      {:>8.5}  |     {:>8.5}", "DC Offset", left.dc_offset(), right.dc_offset());
-    println!("       {:<23} |   {:>8.0} Hz  |  {:>8.0} Hz", "Zero Crossing Rate", left.zero_crossing_rate(), right.zero_crossing_rate());
+    println!("   {:<23}   |   {:>12}   |   {:>12}", "RMS Level", format_db(left.rms()), format_db(right.rms()));
+    println!("   {:<23}   |   {:>12}   |   {:>12}", "Peak Level", format_db(left.peak()), format_db(right.peak()));
+    println!("   {:<23}   |   {:>12}   |   {:>12}", "True Peak", format_db(left.true_peak()), format_db(right.true_peak()));
+    println!("   {:<23}   |   {:>12}   |   {:>12}", "Crest Factor", format_db(left.crest_factor()), format_db(right.crest_factor()));
+    println!("   {:<23}   |   {:>9.5}  V   |   {:>9.5}  V", "DC Offset", left.dc_offset(), right.dc_offset());
+    println!("   {:<23}   |   {:>9} Hz   |   {:>9} Hz", "Zero Crossing Rate", left.zero_crossing_rate().round(), right.zero_crossing_rate().round());
     println!("{}", "─".repeat(70));
-    println!("       {:<23} |     {:>8.2}%  |    {:>8.2}%", "Sample Clipping", left.clipping_samples_quota() * 100.0, right.clipping_samples_quota() * 100.0);
-    println!("       {:<23} |     {:>8.2}%  |    {:>8.2}%", "True Peak Clipping", left.true_clipping_samples_quota() * 100.0, right.true_clipping_samples_quota() * 100.0);
+    println!("   {:<23}   |   {:>9.2}  %   |   {:>9.2}  %", "Sample Clipping", left.clipping_samples_quota() * 100.0, right.clipping_samples_quota() * 100.0);
+    println!("   {:<23}   |   {:>9.2}  %   |   {:>9.2}  %", "True Peak Clipping", left.true_clipping_samples_quota() * 100.0, right.true_clipping_samples_quota() * 100.0);
     println!("{}\n", "─".repeat(70));
     println!("{}", "=".repeat(70));
 

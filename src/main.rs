9192fe78e217b7537b55c505c00712fadc7be96f
Davide Olgiati <davide.olgiati@hotmail.it>
2025-07-25 20:01:06 +0200

    minor ui improvements

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ mod stereo_correlation_builder;
 
 extern crate flac;
 
-use std::env;
+use std::{env, fs};
 use std::fs::File;
+use std::path::Path;
 use flac::StreamReader;
 
 use crate::cli_args::CliArgs;
@@ -26,40 +27,86 @@ fn format_db(value: f32) -> String {
     }
 }
 
-fn print_file_details(filename: &String, file: &FlacFile) {
+fn filename_from_path(filepath: &str) -> String {
+    Path::new(filepath)
+        .file_stem()
+        .and_then(|name| name.to_str())
+        .map(|s| s.to_string())
+        .unwrap()
+}
+
+fn format_file_size(bytes: u64) -> String {
+    const UNITS: &[&str] = &["B", "KB", "MB", "GB", "TB"];
+    let mut size = bytes as f64;
+    let mut unit_index = 0;
+    
+    while size >= 1024.0 && unit_index < UNITS.len() - 1 {
+        size /= 1024.0;
+        unit_index += 1;
+    }
+    
+    if unit_index == 0 {
+        format!("{} {}", bytes, UNITS[unit_index])
+    } else {
+        format!("{:.1} {}", size, UNITS[unit_index])
+    }
+}
+
+fn get_formatted_file_size<P: AsRef<Path>>(path: P) -> String {
+    let metadata = fs::metadata(path).unwrap();
+    let size = metadata.len();
+    format_file_size(size)
+}
+
+fn format_volt(value: f32) -> String {
+    if value > 0.0 {
+        format!("+{:.5}  V", value)
+    } else if value == 0.0 {
+        "0.00000  V".to_string()
+    } else {
+        format!("{:.5}  V", value)
+    }
+}
+
+fn format_hz(value: u32) -> String {
+    format!("{} Hz", value)
+}
+
+fn print_file_details(filename: &str, file: &FlacFile) {
     let seconds = file.duration() % 60_f32;
     let minutes = (file.duration() - seconds) / 60_f32;
     let left = file.left();
     let right = file.right();
 
     println!("{}", "=".repeat(70));
-    println!("SONICPROBE AUDIO ANALYSIS REPORT");
+    println!("{:^70}", "SONICPROBE - AUDIO ANALYSIS REPORT");
     println!("{}\n", "=".repeat(70));
     
     println!("── FILE DETAILS {}\n", "─".repeat(54));
-    println!("   {:<18} : {}", "Filename", *filename);
+    println!("   {:<18} : {}", "Filename", filename_from_path(filename));
+    println!("   {:<18} : {}", "Size", get_formatted_file_size(filename));
+    println!("   {:<18} : {}", "Sample Count", file.samples_count());
     println!("   {:<18} : {:02.0}:{:02.0}", "Duration", minutes, seconds);
-    println!("   {:<18} : {} bit / {} Hz", "Format", file.bit_depth(), file.sample_rate());
-    println!("   {:<18} : {}", "Channels", file.channel_count());
-    println!("   {:<18} : {}\n", "Sample Count", file.samples_count());
+    println!("   {:<18} : {} bit / {}", "Format", file.bit_depth(), format_hz(file.sample_rate()));
     
-    println!("\n── STEREO FIELD ANALYSIS {}\n", "─".repeat(45));
+    println!("\n\n── STEREO FIELD ANALYSIS {}\n", "─".repeat(45));
+    println!("   {:<18} :  {}", "Channels", file.channel_count());
     println!("   {:<18} : {}", "RMS Balance (L/R)", format_db(file.rms_balance()));
-    println!("   {:<18} :  {:.2}\n", "Stereo Correlation", file.stereo_correlation());
+    println!("   {:<18} :  {:.2}", "Stereo Correlation", file.stereo_correlation());
     
-    println!("\n{}", "─".repeat(70));
-    println!("      {:<23}|   {:>12}   |   {:>12}", "CHANNEL ANALYSIS", "LEFT", "RIGHT");
-    println!("{}", "─".repeat(70));
-    println!("   {:<23}   |   {:>12}   |   {:>12}", "RMS Level", format_db(left.rms()), format_db(right.rms()));
-    println!("   {:<23}   |   {:>12}   |   {:>12}", "Peak Level", format_db(left.peak()), format_db(right.peak()));
-    println!("   {:<23}   |   {:>12}   |   {:>12}", "True Peak", format_db(left.true_peak()), format_db(right.true_peak()));
-    println!("   {:<23}   |   {:>12}   |   {:>12}", "Crest Factor", format_db(left.crest_factor()), format_db(right.crest_factor()));
-    println!("   {:<23}   |   {:>9.5}  V   |   {:>9.5}  V", "DC Offset", left.dc_offset(), right.dc_offset());
-    println!("   {:<23}   |   {:>9} Hz   |   {:>9} Hz", "Zero Crossing Rate", left.zero_crossing_rate().round(), right.zero_crossing_rate().round());
-    println!("{}", "─".repeat(70));
-    println!("   {:<23}   |   {:>9.2}  %   |   {:>9.2}  %", "Sample Clipping", left.clipping_samples_quota() * 100.0, right.clipping_samples_quota() * 100.0);
-    println!("   {:<23}   |   {:>9.2}  %   |   {:>9.2}  %", "True Peak Clipping", left.true_clipping_samples_quota() * 100.0, right.true_clipping_samples_quota() * 100.0);
-    println!("{}\n", "─".repeat(70));
+    println!("\n\n{}┬{}┬{}", "─".repeat(29), "─".repeat(18), "─".repeat(21));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "CHANNEL ANALYSIS", "LEFT", "RIGHT");
+    println!("{}┼{}┼{}", "─".repeat(29), "─".repeat(18), "─".repeat(21));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "RMS Level", format_db(left.rms()), format_db(right.rms()));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "Peak Level", format_db(left.peak()), format_db(right.peak()));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "True Peak", format_db(left.true_peak()), format_db(right.true_peak()));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "Crest Factor", format_db(left.crest_factor()), format_db(right.crest_factor()));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "DC Offset", format_volt(left.dc_offset()), format_volt(right.dc_offset()));
+    println!("   {:<23}   │   {:>12}   │   {:>12}", "Zero Crossing Rate", format_hz(left.zero_crossing_rate().round() as u32), format_hz(right.zero_crossing_rate().round() as u32));
+    println!("{}┼{}┼{}", "─".repeat(29), "─".repeat(18), "─".repeat(21));
+    println!("   {:<23}   │   {:>9.5}  %   │   {:>9.5}  %", "Clipping", left.clipping_samples_quota() * 100.0, right.clipping_samples_quota() * 100.0);
+    println!("   {:<23}   │   {:>9.5}  %   │   {:>9.5}  %", "True Clipping", left.true_clipping_samples_quota() * 100.0, right.true_clipping_samples_quota() * 100.0);
+    println!("{}┴{}┴{}", "─".repeat(29), "─".repeat(18), "─".repeat(21));
     println!("{}", "=".repeat(70));
 
 }

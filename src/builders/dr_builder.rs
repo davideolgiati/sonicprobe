9d760026d01bed9cff0a4dc6c799665bd7059fb6
Davide Olgiati <davide.olgiati@hotmail.it>
2025-07-29 14:29:00 +0200

    better dr builder memory management

--- src/builders/dr_builder.rs
+++ src/builders/dr_builder.rs
@@ -1,40 +1,45 @@
-use crate::{audio_utils::to_dbfs, builders::{DRBuilder, RMSBuilder}};
+use crate::{
+    audio_utils::to_dbfs,
+    builders::{DRBuilder, RMSBuilder},
+};
 
 impl DRBuilder {
-        pub fn new(sample_frequency: u32) -> DRBuilder {
-                DRBuilder { 
-                        sample_frequency, 
-                        rms_avarage: 0.0
-                }
+    pub fn new(sample_frequency: u32) -> DRBuilder {
+        DRBuilder {
+            sample_frequency,
+            rms_avarage: 0.0,
         }
+    }
 
-        pub fn add(&mut self, samples: &[f32]) {
-                let mut rms_array: Vec<f32> = Vec::new();
-                let chunk_size = (self.sample_frequency as f32 * 1.5).round() as usize;
-                let reminder = samples.len() % chunk_size;
-                let samples_end = samples.len() - reminder;
-                let mut last_chunk: Vec<f32> = Vec::new();
+    pub fn add(&mut self, samples: &[f32]) {
+        let chunk_size = (self.sample_frequency as f32 * 1.5).round() as usize;
+        let reminder = samples.len() % chunk_size;
+        let samples_end = samples.len() - reminder;
 
-                for chunk in samples[0..samples_end].chunks(chunk_size) {
-                        if !last_chunk.is_empty() {
-                                let mut current_chunk = last_chunk.clone();
-                                current_chunk.append(&mut chunk.to_vec());
-                                let mut rms_builder = RMSBuilder::new();
-                                for sample in current_chunk {
-                                        rms_builder.add(sample);
-                                }
-                                rms_array.push(rms_builder.build())
-                        }
-                        last_chunk = chunk.to_vec()
+        let mut rms_array: Vec<f32> = samples[0..samples_end - chunk_size]
+            .chunks(chunk_size)
+            .zip(samples[chunk_size..samples_end].chunks(chunk_size))
+            .map(|(last_chunk, chunk)| {
+                let mut rms_builder = RMSBuilder::new();
+
+                for sample in last_chunk {
+                    rms_builder.add(*sample);
                 }
 
-                let rms_end = (rms_array.len() as f32 * 0.2).round() as usize;
-                rms_array.sort_by(|a, b| b.partial_cmp(a).unwrap());
-                self.rms_avarage = rms_array[0..rms_end].iter().sum::<f32>() / rms_end as f32;
-        }
+                for sample in chunk {
+                    rms_builder.add(*sample);
+                }
 
-        pub fn build(&self, peak: f32) -> f32 {
-                to_dbfs(peak) - to_dbfs(self.rms_avarage)
-        }
+                rms_builder.build()
+            })
+            .collect();
 
+        let rms_end = (rms_array.len() as f32 * 0.2).round() as usize;
+        rms_array.sort_by(|a, b| b.partial_cmp(a).unwrap());
+        self.rms_avarage = rms_array[0..rms_end].iter().sum::<f32>() / rms_end as f32;
+    }
+
+    pub fn build(&self, peak: f32) -> f32 {
+        to_dbfs(peak) - to_dbfs(self.rms_avarage)
+    }
 }

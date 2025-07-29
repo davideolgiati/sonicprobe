80ee259562cfc09ca68828557616a678c3051eb9
Davide Olgiati <davide.olgiati@hotmail.it>
2025-07-29 09:52:42 +0200

    better dot multiplication performance

--- src/channel/low_pass_filter.rs
+++ src/channel/low_pass_filter.rs
@@ -1,68 +1,46 @@
 use crate::{audio_utils::low_pass_filter, circular_buffer::CircularBuffer};
 
-const NUMTAPS: i16 = 128; // TODO: renedrelo parametrico
+const NUMTAPS: i16 = 128;
 
 pub struct LowPassFilter {
-        coeffs: [f32; 128],
-        window: CircularBuffer<f32>,
-        window_buffer: [f32; 128]
+    coeffs: [f32; 128],
+    window: CircularBuffer<f32>,
+    window_buffer: [f32; 128],
 }
 
 impl LowPassFilter {
-        pub fn new(original_frequency: u32, upsampling_factor: u32) -> LowPassFilter {
-                let cutoff_hz: f32 = (original_frequency as f32) / 2.0;
-                let upsampled_freq: f32 = (original_frequency * upsampling_factor) as f32;
+    pub fn new(original_frequency: u32, upsampling_factor: u32) -> LowPassFilter {
+        let cutoff_hz: f32 = (original_frequency as f32) / 2.0;
+        let upsampled_freq: f32 = (original_frequency * upsampling_factor) as f32;
 
-                let mut coeffs: Vec<f32> = low_pass_filter(
-                        cutoff_hz, 
-                        upsampled_freq, 
-                        NUMTAPS
-                );
+        let mut coeffs: Vec<f32> = low_pass_filter(cutoff_hz, upsampled_freq, NUMTAPS);
 
-                coeffs.reverse();
+        coeffs.reverse();
 
-                let mut coeffs_slice= [0.0f32; 128]; 
-                coeffs_slice.copy_from_slice(&coeffs);
+        let mut coeffs_slice = [0.0f32; 128];
+        coeffs_slice.copy_from_slice(&coeffs);
 
-                LowPassFilter { 
-                        coeffs: coeffs_slice,
-                        window: CircularBuffer::new(
-                                NUMTAPS as usize,
-                                0.0
-                        ),
-                        window_buffer: [0.0f32; 128]
-                }
+        LowPassFilter {
+            coeffs: coeffs_slice,
+            window: CircularBuffer::new(NUMTAPS as usize, 0.0),
+            window_buffer: [0.0f32; 128],
         }
+    }
 
-        pub fn filter(&mut self, sample: f32) -> f32 {
-                self.window.push(sample);
-                self.window_buffer.copy_from_slice(self.window.collect());
+    pub fn filter(&mut self, sample: f32) -> f32 {
+        self.window.push(sample);
+        self.window_buffer.copy_from_slice(self.window.collect());
 
-                dot_product(&self.coeffs, &self.window_buffer)
-        }
+        dot_product(&self.coeffs, &self.window_buffer)
+    }
 }
 
 #[inline]
 fn dot_product(coeffs: &[f32; 128], samples: &[f32; 128]) -> f32 {
-        debug_assert_eq!(coeffs.len(), samples.len());
-    
-        let mut sum = 0.0f32;
-        
-        let chunks = coeffs.len() / 4;
-        let remainder = coeffs.len() % 4;
-        
-        for i in 0..chunks {
-            let base_idx = i * 4;
-            
-            sum += coeffs[base_idx] * samples[base_idx]
-                 + coeffs[base_idx + 1] * samples[base_idx + 1]
-                 + coeffs[base_idx + 2] * samples[base_idx + 2]
-                 + coeffs[base_idx + 3] * samples[base_idx + 3];
-        }
-        
-        for i in (chunks * 4)..(chunks * 4 + remainder) {
-            sum += coeffs[i] * samples[i];
-        }
-        
-        sum
+    coeffs
+        .chunks(32)
+        .zip(samples.chunks(32))
+        .fold(0.0, |acc: f32, (v1, v2)| {
+            acc + v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2] + v1[3] * v2[3]
+        })
 }

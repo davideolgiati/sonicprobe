1e99039ba7e1ff8dd8b2b621070ca5dbdc692f8d
Davide Olgiati <davide.olgiati@hotmail.it>
2025-08-14 09:14:27 +0200

    add actual bit depth tests

--- src/audio_file/analysis/actual_bit_depth.rs
+++ src/audio_file/analysis/actual_bit_depth.rs
@@ -1,6 +1,6 @@
 use crate::{
-    audio_file::{types::BitDepth, Signal},
-    constants::{MAX_16_BIT, MAX_24_BIT, MAX_32_BIT, MAX_8_BIT},
+    audio_file::{Signal, types::BitDepth},
+    constants::{MAX_8_BIT, MAX_16_BIT, MAX_24_BIT, MAX_32_BIT},
     sonicprobe_error::SonicProbeError,
 };
 
@@ -23,7 +23,8 @@ impl super::ActualBitDepth {
 
             let reconstructed_value: i32 = unsafe { (sample * factor).trunc().to_int_unchecked() };
 
-            let sample_depth: u8 = depth.to_bits() - u8::try_from(reconstructed_value.trailing_zeros())?;
+            let sample_depth: u8 =
+                depth.to_bits() - u8::try_from(reconstructed_value.trailing_zeros())?;
 
             if sample_depth > actual_depth {
                 actual_depth = sample_depth;
@@ -41,7 +42,8 @@ impl super::ActualBitDepth {
 
             let reconstructed_value: i32 = unsafe { (sample * factor).trunc().to_int_unchecked() };
 
-            let sample_depth: u8 = depth.to_bits() - u8::try_from(reconstructed_value.trailing_zeros())?;
+            let sample_depth: u8 =
+                depth.to_bits() - u8::try_from(reconstructed_value.trailing_zeros())?;
 
             if sample_depth > actual_depth {
                 actual_depth = sample_depth;
@@ -53,5 +55,62 @@ impl super::ActualBitDepth {
         }
 
         Ok(actual_depth)
+    }
+}
+
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    use crate::{
+        audio_file::{analysis::ActualBitDepth, types::BitDepth},
+        constants::{MAX_16_BIT, MAX_8_BIT},
+    };
+
+    #[test]
+    fn matching_size() {
+        let left = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u8| f64::from(*val) / MAX_8_BIT)
+            .collect();
+        let right = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u8| f64::from(*val) / MAX_8_BIT)
+            .collect();
+        let depth = BitDepth::new(8).unwrap();
+        let res = ActualBitDepth::process(&left, &right, depth).unwrap();
+
+        assert_eq!(res, 8u8);
+    }
+
+        #[test]
+    fn padded_data() {
+        let left = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u16| f64::from(*val << 8) / MAX_16_BIT)
+            .collect();
+        let right = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u16| f64::from(*val << 8) / MAX_16_BIT)
+            .collect();
+        let depth = BitDepth::new(16).unwrap();
+        let res = ActualBitDepth::process(&left, &right, depth).unwrap();
+
+        assert_eq!(res, 8u8);
+    }
+
+            #[test]
+    fn mixed_data() {
+        let left = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u16| f64::from(*val << 1) / MAX_16_BIT)
+            .collect();
+        let right = [10, 20, 30, 111]
+            .iter()
+            .map(|val: &u16| f64::from(*val << 3) / MAX_16_BIT)
+            .collect();
+        let depth = BitDepth::new(16).unwrap();
+        let res = ActualBitDepth::process(&left, &right, depth).unwrap();
+
+        assert_eq!(res, 15u8);
     }
 }

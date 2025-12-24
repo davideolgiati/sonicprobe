f25382a37d56867630de4ccad7ef833d01f3233e
Davide Olgiati <davide.olgiati@hotmail.it>
2025-12-24 09:15:15 +0100

    refactoring dsp upsample chain pt. 1

--- sonicprobe-core/src/dsp.rs
+++ sonicprobe-core/src/dsp.rs
@@ -12,33 +12,42 @@ pub fn upsample_chain(
 ) -> Result<(Decibel, u64), SonicProbeError> {
     let mut low_pass = LowPassFilter::new(source_sample_rate)?;
 
-    let mut peak_h = f64::MIN;
-    let mut peak_l = f64::MAX;
-    let mut clipping_samples = 0u64;
+    let mut peak = f64::MIN;
+    let mut clipping_samples_count = 0u64;
 
     for i in 0..source.len() - 12 {
         low_pass.submit(&source[i..i + 12]);
 
-        for &value in low_pass.get_buffer() {
-            if is_distorted(value) {
-                clipping_samples += 1;
-            }
-
-            if value > peak_h {
-                peak_h = value;
-            } else if value < peak_l {
-                peak_l = value;
-            } 
+        for sample in low_pass.get_buffer() {
+            (peak, clipping_samples_count) = process_upsampled_sample(
+                sample, &peak, &clipping_samples_count
+            ) 
         }
     }
 
-    let peak = {
-        if peak_l.abs() > peak_h {
-            peak_l.abs()
+    Ok((Decibel::new(peak), clipping_samples_count))
+}
+
+pub fn process_upsampled_sample(
+    sample: &f64, peak: &f64, clipping_samples_count: &u64
+) -> (f64, u64) {
+    let abs_value = sample.abs();
+
+    let new_clipping_samples_count = {
+        if is_distorted(abs_value) {
+            clipping_samples_count + 1
         } else {
-            peak_h
+            *clipping_samples_count
         }
     };
 
-    Ok((Decibel::new(peak), clipping_samples))
+    let new_peak = {
+        if abs_value > *peak {
+            abs_value
+        } else {
+            *peak
+        }
+    };
+
+    return (new_peak, new_clipping_samples_count)
 }

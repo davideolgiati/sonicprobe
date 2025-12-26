4b71cf66446660004d0be519e857f15b20b55f21
Davide Olgiati <davide.olgiati@hotmail.it>
2025-12-26 11:12:23 +0100

    split process_upsampled_sample into sub functions, add tests

--- sonicprobe-core/src/dsp.rs
+++ sonicprobe-core/src/dsp.rs
@@ -24,26 +24,147 @@ pub fn upsample_chain(
     Ok((Decibel::new(peak), clipping_samples_count))
 }
 
-pub fn process_upsampled_sample(
+fn process_upsampled_sample(
     sample: &f64, peak: &f64, clipping_samples_count: &u64
 ) -> (f64, u64) {
     let abs_value = sample.abs();
 
-    let new_clipping_samples_count = {
-        if is_distorted(abs_value) {
-            clipping_samples_count + 1
-        } else {
-            *clipping_samples_count
-        }
+    let new_clipping_count = match update_clipping_count(
+        clipping_samples_count, &abs_value
+    ) {
+        Some(result) => result,
+        None => *clipping_samples_count
     };
 
-    let new_peak = {
-        if abs_value > *peak {
-            abs_value
-        } else {
-            *peak
-        }
+    let new_peak = match update_peak_value(peak, &abs_value){
+        Some(result) => result,
+        None => *peak
     };
 
-    return (new_peak, new_clipping_samples_count)
+    return (new_peak, new_clipping_count)
+}
+
+fn update_clipping_count(current_count: &u64, sample: &f64) -> Option<u64> {
+    if is_distorted(*sample) {
+        return Some(*current_count + 1)
+    }
+
+    None
+}
+
+fn update_peak_value(current_peak: &f64, sample: &f64) -> Option<f64> {
+    if *sample > *current_peak {
+        return Some(*sample)
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+    #[test]
+    fn update_clipping_count_increase() {
+        let mut rng = rand::rng();
+        let clipping_sample: f64 = rng.random_range(1.0..2.0);
+        let current_clipping_sample_count = 0;
+
+        let result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &clipping_sample
+        ); 
+
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn update_clipping_count_no_increase() {
+        let mut rng = rand::rng();
+        let sample: f64 = rng.random_range(-0.9..0.9);
+        let current_clipping_sample_count = 0;
+
+        let result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn update_clipping_count_edge_cases() {
+        let mut sample: f64 = 1.0;
+        let current_clipping_sample_count = 0;
+
+        let mut result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, Some(1));
+
+        sample = -1.0;
+        result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, Some(1));
+
+        sample = 0.9999999;
+        result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, None);
+
+        sample = -0.9999999;
+        result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, None);
+
+        sample = 1.0000001;
+        result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, Some(1));
+
+        sample = -1.0000001;
+        result = update_clipping_count(
+            &current_clipping_sample_count, 
+            &sample
+        ); 
+
+        assert_eq!(result, Some(1));
+    }
+
+    #[test]
+    fn update_peak_value_change() {
+        let mut rng = rand::rng();
+        let current_peak = rng.random_range(0.0..0.5);
+        let sample = rng.random_range(0.50000001..1.0);
+
+        let result = update_peak_value(&current_peak, &sample);
+
+        assert_eq!(result, Some(sample))
+    }
+
+    #[test]
+    fn update_peak_value_no_change() {
+        let mut rng = rand::rng();
+        let current_peak = rng.random_range(0.0..0.5);
+        let sample = rng.random_range(-1.0..current_peak);
+
+        let result = update_peak_value(&current_peak, &sample);
+
+        assert_eq!(result, None)
+    }
 }
